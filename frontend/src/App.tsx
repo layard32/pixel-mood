85619a13f80846e85dabd291e6ab62d329306899
layard <114928369+layard32@users.noreply.github.com>
2026-01-12 13:30:10 +0100

    build: calendar component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,42 @@
 import "@mantine/core/styles.css";
-import { MantineProvider } from "@mantine/core";
+import { MantineProvider, AppShell, Burger } from "@mantine/core";
 import { theme } from "./theme";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useDisclosure } from "@mantine/hooks";
 
 const queryClient = new QueryClient();
 
 export default function App() {
+  const [opened, { toggle }] = useDisclosure();
+
   return (
     <MantineProvider theme={theme}>
-      <QueryClientProvider client={queryClient}></QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        <AppShell
+          padding="md"
+          header={{ height: 60 }}
+          navbar={{
+            width: 300,
+            breakpoint: "sm",
+            collapsed: { mobile: !opened },
+          }}
+        >
+          <AppShell.Header>
+            <Burger
+              opened={opened}
+              onClick={toggle}
+              hiddenFrom="sm"
+              size="sm"
+            />
+
+            <div>Logo</div>
+          </AppShell.Header>
+
+          <AppShell.Navbar>Navbar</AppShell.Navbar>
+
+          <AppShell.Main>Main</AppShell.Main>
+        </AppShell>
+      </QueryClientProvider>
     </MantineProvider>
   );
 }
---
+++ frontend/src/components/calendar.tsx
@@ -0,0 +1,5 @@
+import { Heatmap } from "@mantine/charts";
+
+export function Demo() {
+  //   return <Heatmap startDate="2024-02-16" endDate="2025-02-16" />;
+}
