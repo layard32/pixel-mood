d770651fb6f4840faa61e0dc30641903abe0153c
layard <114928369+layard32@users.noreply.github.com>
2026-01-12 16:14:18 +0100

    fix: calendar style

--- frontend/src/components/calendar.tsx
+++ frontend/src/components/calendar.tsx
@@ -7,44 +7,56 @@ export function Calendar() {
 
   return (
     // usiamo Stack per disporre i mesi in colonna
-    <Stack gap={4}>
-      {/* iteriamo sulle chiavi della mappa, cioè sui mesi */}
-      {Object.entries(daysByMonth).map(([month, days]) => (
-        // per ogni mese creiamo un Box parente (con Box)
-        <Box
-          key={month}
-          style={{ display: "flex", gap: "16px", alignItems: "flex-start" }}
-        >
-          <Text size="sm" fw={500} style={{ minWidth: "80px" }}>
-            {month}
-          </Text>
-
-          {/* creiamo un primo Box figlio che conterrà i giorni del mese 
-          (a loro volte come Box) */}
+    <Box
+      style={{ display: "flex", justifyContent: "center", paddingTop: "15px" }}
+    >
+      <Stack gap={10}>
+        {/* iteriamo sulle chiavi della mappa, cioè sui mesi */}
+        {Object.entries(daysByMonth).map(([month, days]) => (
+          // per ogni mese creiamo un Box parente (con Box)
           <Box
+            key={month}
             style={{
               display: "flex",
-              flexWrap: "wrap",
-              gap: "4px",
+              gap: "16px",
+              alignItems: "flex-start",
             }}
           >
-            {days.map((day) => (
-              <Box
-                key={day}
-                w={40}
-                h={40}
-                bg="gray.2"
-                onClick={() => console.log(day)}
-                style={{
-                  borderRadius: "5px",
-                  cursor: "pointer",
-                }}
-                title={day}
-              />
-            ))}
+            <Text
+              fz={{ base: "sm", sm: "md", md: "lg" }}
+              fw={500}
+              miw={{ base: "70px", sm: "95px", md: "110px" }}
+              style={{ flexShrink: 0 }}
+            >
+              {month}
+            </Text>
+
+            {/* creiamo un primo Box figlio che conterrà i giorni del mese 
+          (a loro volte come Box) */}
+            <Box
+              style={{
+                display: "flex",
+                flexWrap: "wrap",
+                gap: "4px",
+              }}
+            >
+              {days.map((day) => (
+                <Box
+                  key={day}
+                  w={40}
+                  h={40}
+                  bg="gray.2"
+                  style={{
+                    borderRadius: "10px",
+                    cursor: "pointer",
+                  }}
+                  title={day}
+                />
+              ))}
+            </Box>
           </Box>
-        </Box>
-      ))}
-    </Stack>
+        ))}
+      </Stack>
+    </Box>
   );
 }
